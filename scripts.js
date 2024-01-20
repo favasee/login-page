822bc30ded28440a22c4d8fca77bd5d6fe53e1c7
favasee <156898469+favasee@users.noreply.github.com>
2024-01-20 21:10:56 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,28 @@
+function runAnimations() {
+  setTimeout(() => {
+    document.querySelector("#id-38133").classList.add("animationClass-38133");
+    document.querySelector("#id-38135").classList.add("animationClass-38135");
+    document.querySelector("#id-38134").classList.add("animationClass-38134");
+    document.querySelector("#id-38136").classList.add("animationClass-38136");
+    document.querySelector("#id-38139").classList.add("animationClass-38139");
+    document.querySelector("#id-38140").classList.add("animationClass-38140");
+    document
+      .querySelector(".node-38141-0")
+      .classList.add("animationClass-38141-38141-0");
+    document.querySelector("#id-38141").classList.add("animationClass-38141");
+    document
+      .querySelector(".node-38142-0")
+      .classList.add("animationClass-38142-38142-0");
+    document.querySelector("#id-38142").classList.add("animationClass-38142");
+    document
+      .querySelector(".node-38143-0")
+      .classList.add("animationClass-38143-38143-0");
+    document.querySelector("#id-38143").classList.add("animationClass-38143");
+    document
+      .querySelector(".node-38144-0")
+      .classList.add("animationClass-38144-38144-0");
+    document.querySelector("#id-38144").classList.add("animationClass-38144");
+    document.querySelector("#id-4689").classList.add("animationClass-4689");
+  }, 800);
+}
+runAnimations();
